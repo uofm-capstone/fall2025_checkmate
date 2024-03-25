0be5565842a2a148ba964d3dbf5b264f559359d8
Naitik Kaythwal <naitikkaythwal@gmail.com>
2024-03-25 18:20:35 -0500

    Fixed alignment issues

--- app/controllers/semesters_controller.rb
+++ app/controllers/semesters_controller.rb
@@ -295,8 +295,6 @@ class SemestersController < ApplicationController
         @full_questions = client_data[:full_questions]
         @cliSurvey = client_data[:cliSurvey]
         @flags = flags
-        @start_dates, @end_dates, @team_names, @repo_owners, @repo_names, @access_tokens = get_git_info()
-
 
         render :team
     end
