e072cf575c866f0d2b9f9a810adb61ee726d630f
hrizeq17 <hrizeq@memphis.edu>
2024-04-16 23:39:10 -0500

    Fix Procfile casing

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,8 +1,8 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
 
-const channels = require.context('.', true, /_channel\.js$/)
-channels.keys().forEach(channels)
+//const channels = require.context('.', true, /_channel\.js$/)
+//channels.keys().forEach(channels)
 index.listen(process.env.PORT || 5000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
