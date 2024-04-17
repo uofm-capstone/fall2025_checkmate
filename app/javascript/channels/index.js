8ceb9ef8ce2385b602fc55a0cf2123802137b26b
hrizeq17 <hrizeq@memphis.edu>
2024-04-16 23:10:10 -0500

    node engine

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,17 +1,8 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
 
-//const channels = require.context('.', true, /_channel\.js$/)
-//channels.keys().forEach(channels)
-//app.listen(process.env.PORT || 3000, function(){
-  //  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
- // });
-
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('index.html');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(index);
-}).listen(5000);
+const channels = require.context('.', true, /_channel\.js$/)
+channels.keys().forEach(channels)
+index.listen(process.env.PORT || 5000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
