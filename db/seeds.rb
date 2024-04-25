e170d934a83bd0fdf592ca4ce1df77197888f7f7
hrizeq17 <hrizeq@memphis.edu>
2024-04-25 14:15:04 -0500

    update seed again

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,27 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-user = User.find_or_create_by(email: "email@email.com") do |user|
+User.find_or_create_by(email: "email@email.com") do |user|
   user.password = "123456"
-  puts "Creating user with email@email.com"
 end
-
-puts "User created: #{user.id}" if user.persisted?
 
 # Admin for Professor Amy Cook
-amy = User.find_or_create_by(email: "AmyCook@admin.com") do |user|
+User.find_or_create_by(email: "AmyCook@admin.com") do |user|
   user.password = "Admin1"
   user.admin = true
-  puts "Creating admin for Amy Cook"
 end
-
-puts "Admin Amy created: #{amy.id}" if amy.persisted?
 
 # Admin for Professor Brandon Booth
-brandon = User.find_or_create_by(email: "BrandonBooth@admin.com") do |user|
+User.find_or_create_by(email: "BrandonBooth@admin.com") do |user|
   user.password = "Admin2"
   user.admin = true
-  puts "Creating admin for Brandon Booth"
 end
-
-puts "Admin Brandon created: #{brandon.id}" if brandon.persisted?
