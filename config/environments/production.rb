b994682163049c35a0e70d10804238a5646e1e02
Tyler Howell <tyler.howell1202@gmail.com>
2025-04-01 15:09:03 -0500

    Formal feedback - Tyler

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << /.*\.run\.app/
+  config.hosts << "tag-app-service-989253787422.us-central1.run.app"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
