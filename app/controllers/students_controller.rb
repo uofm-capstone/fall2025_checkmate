71bb6ae854718ff0e3c67acaef41426b7d874635
Derron0325 <dmdowdy@memphis.edu>
2025-10-05 23:28:06 -0500

    revised controllers

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,24 @@
 # app/controllers/students_controller.rb
 class StudentsController < ApplicationController
+  skip_before_action :ensure_semester_exists, raise: false rescue nil
+
+  before_action :set_student, only: %i[show edit update destroy]
+
+  # GET /students
   def index
-    @students = Student.all
+    @students = Student.order(Arel.sql('LOWER(full_name)'))
+  end
+
+  # GET /students/:id
+  def show; end
+
+  # GET /students/new
+  def new
+    @student = Student.new
+    @teams = Team.order(:name)
+  end
+
+  # POST /students
   def create
     @student = Student.new(student_params)
     if @student.save
@@ -10,6 +27,39 @@ class StudentsController < ApplicationController
       render :new
     end
   end
+
+  # GET /students/:id/edit
+  def edit; end
+
+  # PATCH/PUT /students/:id
+  def update
+    if @student.update(student_params)
+      redirect_to students_path, notice: "Student updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /students/:id
+  def destroy
+    @student.destroy
+    redirect_to students_path, notice: "Student removed."
+  end
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:full_name, :email, :github_username, :team_name)
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do
+    redirect_to students_path, alert: "Student not found."
+  end
+end
 
   private
 
