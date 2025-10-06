cac495aa34142c5f71ccba01cb0dba8d5f241773
Derron0325 <dmdowdy@memphis.edu>
2025-10-05 22:26:37 -0500

    Revised models

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ApplicationRecord
+  validates :full_name, :email, :github_username, :team_name, presence: true
+  
   has_many :student_teams, dependent: :destroy
   has_many :teams, through: :student_teams
 end
