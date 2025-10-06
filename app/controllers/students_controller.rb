d63b062a00efe157313f9b9b5fdc45aa11569654
Jacob Hensley <jacobdlhensley@gmail.com>
2025-10-05 20:09:14 -0500

    Finished 'new' pop up for teams

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,18 +1,18 @@
 # app/controllers/students_controller.rb
 class StudentsController < ApplicationController
-    def create
-      @student = Student.new(student_params)
-      if @student.save
-        redirect_to semester_classlist_path(@student.semester), notice: 'Student was successfully added.'
-      else
-        render :new
-      end
-    end
-  
-    private
-  
-    def student_params
-      params.require(:student).permit(:name, :semester_id)
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to semester_classlist_path(@student.semester), notice: 'Student was successfully added.'
+    else
+      render :new
     end
   end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :semester_id)
+  end
+end
   
