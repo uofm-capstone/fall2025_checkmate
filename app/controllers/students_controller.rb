4ccb4c8273690bd88c950c7b57d31c63d0950e2a
Derron0325 <dmdowdy@memphis.edu>
2025-10-06 00:23:50 -0500

    revised views and controllers

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,6 +5,7 @@ class StudentsController < ApplicationController
 
   def index
     @students = Student.includes(:semester).order(Arel.sql('LOWER(full_name)'))
+    @students = Student.new
   end
 
   def show; end
