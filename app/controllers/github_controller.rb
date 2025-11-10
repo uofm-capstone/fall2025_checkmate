0075053314b61519342e01b345f1da03ae34bc08
Zachery Nolan <zacherynolan@yahoo.com>
2025-11-10 16:58:17 -0600

    Modified Github controller

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,4 +1,14 @@
 class GithubController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def webhook
+    payload = request.body.read
+
+    data = JSON.parse(payload)
+    if data['commits']
+      repo = data['repository']['full_name']
+      commit_count = data['commits'].size
+    end
+
+    head :ok
   end
 end
