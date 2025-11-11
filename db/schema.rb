3e17c0bd98868e9f3c27509ffd7a1b9d5f0b6227
Zachery Nolan <zacherynolan@yahoo.com>
2025-11-11 14:12:30 -0600

    Deleted repo model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_10_04_023940) do
+ActiveRecord::Schema[7.0].define(version: 2025_11_11_195941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,13 @@ ActiveRecord::Schema[7.0].define(version: 2025_10_04_023940) do
     t.datetime "updated_at", null: false
     t.index ["semester_id"], name: "index_classlists_on_semester_id"
     t.index ["student_id"], name: "index_classlists_on_student_id"
+  end
+
+  create_table "repos", force: :cascade do |t|
+    t.string "name"
+    t.integer "commit_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "repositories", force: :cascade do |t|
