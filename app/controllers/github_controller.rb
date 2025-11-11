a8539d527e344d339c65f013027850990f6d287c
Zachery Nolan <zacherynolan@yahoo.com>
2025-11-11 13:57:34 -0600

    Modified Github controller

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -3,12 +3,24 @@ class GithubController < ApplicationController
   def webhook
     payload = request.body.read
 
+    event = request.headers['X-GitHub-Event']
     data = JSON.parse(payload)
-    if data['commits']
-      repo = data['repository']['full_name']
-      commit_count = data['commits'].size
+  
+    case event 
+    when 'push'
+      handle_push_event(data)
     end
 
     head :ok
+  end
+
+  private 
+  
+  def handle_push_event(data)
+    repo_name = data['repository']['full_name']
+    commit_count = data['commits'].size
+
+    repo = Repository.find_or_create_by(name: repo_name)
+    repo.increment!(:commit_count, commit_count)
   end
 end
